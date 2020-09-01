7aba8a399d39da5d6d3499b975284d958d9b6359
reyalsyssup <63643459+reyalsyssup@users.noreply.github.com>
2020-09-01 22:00:16 +1000

    Able to write contacts to a file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <string>
 #include <regex>
+#include <fstream>
 
-const std::regex emailPattern("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
+const std::regex emailPattern("^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$");
 
 void toLowercase(std::string &string, int length) {
     for(int i = 0; i < length; i++) {
@@ -28,24 +29,28 @@ int main() {
         std::string option;
         std::cin >> option;
         if(option == "new") {
-            std::string firstName;
-            std::string lastName;
-            std::string email;
-            std::string phoneNum;
+            std::string fullName, email, phoneNum;
             std::cout << "To leave a field blank, type: null\n";
-            std::cout << "First name of new contact > ";
-            std::cin >> firstName;
-            std::cout << "Last name of new contact > ";
-            std::cin >> lastName;
+            std::cout << "Full name of new contact > ";
+            // using cin.ignore() to ignore the newline created by the previous input
+            std::cin.ignore();
+            std::getline(std::cin, fullName);
             getEmail(email);
+            // wont check phone number format using regex due to different countries having different formats for phone numbers
             std::cout << "Phone number of new contact > ";
             std::cin >> phoneNum;
+            toLowercase(fullName, fullName.length());
             
-            toLowercase(firstName, firstName.length());
-            toLowercase(lastName, lastName.length());
-
-            std::cout << firstName;
-            return 0;
+            // write new contact to contacts file
+            std::ofstream file("contacts.txt", std::ios::app);
+            if(!file.is_open()) {
+                std::cout << "Failed to write new contact to save file\n";
+                break;
+            }
+            file << fullName << std::endl;
+            file << email << std::endl;
+            file << phoneNum << std::endl;
+            file << std::endl;
         }
     }
 }
